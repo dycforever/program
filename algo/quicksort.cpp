3123c0505f77090f9366d1516bbe9ac8b9a1b375
ubuntu12.04 <dyc@ubuntu.(none)>
2014-03-03 08:18:44 -0800

    add generate random int and uniq

--- algo/quicksort.cpp
+++ algo/quicksort.cpp
@@ -5,7 +5,8 @@ using namespace std;
 using namespace dyc;
 
 template<typename T>
-void print(T* array, int len){
+void print(vector<T> array) {
+    int len = array.size();
     for(int i=0; i<len; ++i){
         cout<<array[i];
         int value = i & 0xf;
@@ -18,8 +19,9 @@ void print(T* array, int len){
 }
 
 template<typename T>
-int check(T* array, int len){
-    NOTICE("begin check array: %p , len: %d", array, len);
+int check(vector<T> array) {
+    int len = array.size();
+//    NOTICE("begin check array: %p , len: %d", array, len);
     for(int i=0; i<len-1; ++i){
         if ( array[i] > array[i+1] ) {
             FATAL("found erroe array[%d] > array[%d]", i, i+1);
@@ -30,18 +32,44 @@ int check(T* array, int len){
     return 0;
 }
 
-int convert(vector<string>& vec, int* numbers){
-    
-    int i=0;
+int convert(vector<string>& vec, vector<int>& numbers){
     for(vector<string>::iterator iter = vec.begin();
             iter != vec.end(); ++iter){
         int n = atoi((*iter).c_str()); 
-        numbers[i] = n;
-        ++i;
+        numbers.push_back(n);
     }
 }
 
-int quicksort(int* numbers, int size){
+int heapsort(int* numbers, int size) {
+
+}
+
+void buildHeap() {
+    for (int i=0; i<size; ++i) {
+        heapfy(numbers, i+1);
+    }
+}
+
+void heapfy (int* numbers,int last) {
+    int now = last;
+    int left = last-1;
+    int right = last;
+    int top = last/2 - 1;
+    while (top > 0) {
+        if (numbers[left] > numbers[top]) {
+            swap(numbers[left], numbers[top]);
+        }
+        if (numbers[right] > numbers[top]) {
+            swap(numbers[right], numbers[top]);
+        }
+        last /= 2;
+        left = last-1;
+        right = last;
+        top = last/2 - 1;
+    }
+}
+
+int quicksort(int* numbers, int size) {
     if( size <= 1){
         return 0;
     }
@@ -49,16 +77,10 @@ int quicksort(int* numbers, int size){
     int low = 0;
     int high = size;
     while(low < high){
-        while ( numbers[--high] > privot) {
-            if (0 == high) {
-                break;
-            }
+        while ( numbers[--high] > privot && low != high) {
         }
 
-        while ( numbers[++low] <= privot) {
-            if (low == size) {
-                break;
-            }
+        while ( numbers[++low] <= privot && low != high) {
         }
 
         if( low < high ){
@@ -71,6 +93,19 @@ int quicksort(int* numbers, int size){
     quicksort(numbers+high+1, size-high-1);
 }
 
+template<class T>
+int uniq(vector<T>& array) {
+    size_t size = array.size();
+    size_t real = 0;
+    for (size_t i=0; i<size; ++i) {
+        if (i>0 && array[i] == array[i-1]) {
+            continue;
+        }
+        array[real++] = array[i];
+    }
+    array.resize(real);
+}
+
 int main(){
     string str;
     vector<string> vec;
@@ -80,13 +115,17 @@ int main(){
                 istream_iterator<string>(),
                 back_inserter<vector<string> >(vec));
     }
-    int* numbers = NEW int[vec.size()];
+    vector<int> numbers;
     convert(vec, numbers);
     NOTICE("before sort:");
-//    print<int>(numbers, vec.size());
-//    copy(numbers.begin(), numbers.end(), ostream_iterator<int>(cout, "\n"));
-    quicksort(numbers, vec.size());
+    print(numbers);
+
+    quicksort(numbers.data(), numbers.size());
     NOTICE("after sort:");
-    check<int>(numbers, vec.size());
+    print(numbers);
+    check<int>(numbers);
+
+    uniq(numbers);
+    print(numbers);
     return 0;
 }
