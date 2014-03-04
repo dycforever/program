fa75279c063e73bc02ac55122ebdf1a499178897
dycforever <dycforever@126.com>
2014-03-04 14:24:40 +0800

    fix 3sum

--- algo/3sum.cpp
+++ algo/3sum.cpp
@@ -1,4 +1,3 @@
-
 #include <assert.h>
 #include <iostream>
 #include <vector>
@@ -13,6 +12,7 @@ using namespace std;
 template <typename T>
 void PRINT_ELEMENTS(T coll){
     copy(coll.begin(), coll.end(), ostream_iterator<typename T::value_type>(cout, " "));
+    cout << endl;
 }
 
 class Solution {
@@ -21,8 +21,46 @@ class Solution {
             int size = num.size();
             int left = 0;
             int right = size-1;
+            int mid = 0;
             while (left <= right) {
-                int mid = (left + right) / 2;
+                mid = (left + right) / 2;
+                if (num[mid] == target) {
+                    return mid;
+                } else if (num[mid] < target) {
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
+            }
+            if (num[mid] < target) {
+                return mid;
+            }
+            return mid-1;
+        }
+
+        int binary_find_lt(const vector<int> &num, int target) {
+            int size = num.size();
+            int left = 0;
+            int right = size-1;
+            int mid = 0;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (num[mid] <= target) {
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
+            }
+            if (num[mid] > target) {
+                return mid;
+            }
+            return mid+1;
+        }
+
+        int find_target(const vector<int> &num, int left, int right, int target) {
+            int mid = (left + right) / 2;
+            while (left <= right) {
+                mid = (left + right) / 2;
                 if (num[mid] == target) {
                     return mid;
                 } else if (num[mid] < target) {
@@ -34,59 +72,39 @@ class Solution {
             return -1;
         }
 
-        int find_target(const vector<int> &num, int oleft, int oright) {
-            int left = oleft;
-            int right = oright;
-            cout << "left: " << left << " right: " << right << endl;
-            int target = -(num[left] + num[right]);
-            while (left < right) {
-                int mid = (left + right) / 2;
-//                cout << "mid: " << mid << endl;
-//                cout << "left: " << left << " right: " << right << endl;
-                if (num[mid] == target && mid != oleft && mid != oright) {
-                    return mid;
-                } else if (num[mid] < target) {
-                    left = mid+1;
-                } else {
-                    right = mid-1;
-                }
-            }
-            return -1;
-        }
         // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<vector<int> > threeSum(vector<int> &num) {
-            sort(num.begin(), num.end());
             vector<vector<int> > solution;
-            int zero_idx = 9;
-            assert(num[zero_idx] == 0);
+            int size = num.size();
+            if (size < 3) {
+                return solution;
+            }
+            sort(num.begin(), num.end());
+//            PRINT_ELEMENTS(num);
+            int zero_idx = binary_find(num, 0);
+            if (zero_idx < 0 || zero_idx >= size) {
+                return solution;
+            }
+            assert(num[zero_idx] <= 0);
             int left = 0;
-            int right = num.size() - 1;
-            while (true) {
-                int target = find_target(num, left, right);
-                if (target > 0) {
-                    vector<int> tmp;
-                    tmp.push_back(num[left]);
-                    tmp.push_back(num[target]);
-                    tmp.push_back(num[right]);
-                    solution.push_back(tmp);
-                }
-                if(left < zero_idx) {
-                    ++left;
-                }
 
-                target = find_target(num, left, right);
-                if (target > 0) {
-                    vector<int> tmp;
-                    tmp.push_back(num[left]);
-                    tmp.push_back(num[target]);
-                    tmp.push_back(num[right]);
-                    solution.push_back(tmp);
+            int right = size;
+            for (left = 0; left <= zero_idx; ++left) {
+                if (left > 0 && num[left] == num[left-1]) {
+                    continue;
                 }
-                if (right > zero_idx) {
-                    --right;
-                }
-                if (left == zero_idx && right == zero_idx) {
-                    break;
+                for (right = size-1;right >= zero_idx; --right) {
+                    if (right < size-1 && num[right] == num[right+1]) {
+                        continue;
+                    }
+                    int target = find_target(num, left+1, right-1, -(num[left] + num[right]));
+                    if (target > 0) {
+                        vector<int> tmp;
+                        tmp.push_back(num[left]);
+                        tmp.push_back(num[target]);
+                        tmp.push_back(num[right]);
+                        solution.push_back(tmp);
+                    }
                 }
             }
             return solution;
@@ -96,21 +114,16 @@ class Solution {
 
 int main () {
     Solution s;
-    int array0[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> vec0(array0, array0 + sizeof(array0)/sizeof(array0[0]));
-    for (uint32_t i=0; i<10; ++i) {
-        cout << s.binary_find(vec0, i) << endl;
-    }
-    return 0;
-
-    int array[] = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//    int array[] = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//    int array[] = {-1, 0, 1, 2, -1, -4};
+//    int array[] = {1, 1, 1};
+//    int array[] = {0,0,0,0,-1, -1, -1};
+//    int array[] = {0,-5,3,-4,1,3,-4,-2,-2,-2,0,3,0,1,-4,-2,0};
+    int array[] = {-1,4,0,0,-4,-5,-4,-5,0,2,-5,2,-2};
     vector<int> vec(array, array + sizeof(array)/sizeof(array[0]));
-    PRINT_ELEMENTS(vec);
-    cout << endl;
 
     vector<vector<int> > r = s.threeSum(vec);
     for (vector<vector<int> >::iterator it = r.begin(); it != r.end(); ++it) {
         PRINT_ELEMENTS(*it);
-        cout << endl;
     }
 }
