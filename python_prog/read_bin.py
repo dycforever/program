d72173e93aeb840a353743171d4c4da481fa6b74
ubuntu12.04 <dyc@ubuntu.(none)>
2014-03-29 00:01:08 -0700

    modify a little

--- python_prog/read_bin.py
+++ python_prog/read_bin.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
 import struct
-fp = open("./a.out", "rb")
-bytes=fp.read()
-if bytes[0]==0x7f:
-    print("0:%x" %(bytes[0]))
-else:
-    print("unknow 0")
-    
-if bytes[1]==0x45:
-    print("1:%x" %(bytes[1]))
-else:
-    print("unknow %x")
-    
+def read_bin():
+    """
+    this is a doc string
+    """
+    fp = open("./a.out", "rb")
+    bytes=fp.read()
+    if bytes[0]==0x7f:
+        print("0:%x" %(bytes[0]))
+    else:
+        print("unknow 0")
+
+    if bytes[1]==0x45:
+        print("1:%x" %(bytes[1]))
+    else:
+        print("unknow %x")
+
+if __name__ == "__main__":
+    print("read_bin()'s doc: %s" % read_bin.__doc__)
