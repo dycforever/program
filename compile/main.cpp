ed9c620ab1f8b43e762102f14c4925f37637a5f6
dyc <dyc@localhost.localdomain>
2015-01-01 23:15:57 +0800

    add code

--- compile/main.cpp
+++ compile/main.cpp
@@ -1,10 +1,46 @@
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <iostream>
+#include <fstream>
 
-int main()
+std::string filename;
+
+int parseArg(int argc, char** argv) {
+    int c;
+    while((c = getopt(argc, argv, "f:")) != -1) {
+        switch(c) {
+            case 'f':
+                filename = std::string(optarg);
+                break;
+            default:
+                std::cerr << "parse options failed" << std::endl;
+                exit(-1);
+        }
+    }
+} // parseArg()
+
+size_t ReadAndCount(std::istream& in)
 {
     std::string line;
-    while (getline(std::cin, line)) {
-        std::cout << line << std::endl;
+    size_t count = 0;
+    while (getline(in, line)) {
+        ++count;
     }
+    return count;
 }
+
+int main(int argc, char** argv) {
+    parseArg(argc, argv);
+    size_t count = 0;
+    if (filename == "") {
+        count = ReadAndCount(std::cin);
+    } else {
+        std::ifstream in(filename.c_str());
+        count = ReadAndCount(in);
+    }
+
+    std::cout << count << std::endl;
+}
+
+
