099d4344aaef05a4de0bb35b0e83f000a3674a51
dyc <dyc@localhost.localdomain>
2015-08-21 05:39:45 +0800

    add cookbook

--- openssl/doc.sh
+++ openssl/doc.sh
@@ -1,27 +1,5 @@
 openssl x509 -x509toreq -in fd.crt -out fd.csr -signkey fd.key
 
-create cert:
-openssl x509 -req -days 365 -in fd.csr -signkey fd.key -out fd.crt
-
-create cert without csr:
-openssl req -new -x509 -days 365 -key fd.key -out fd.crt
-
-create cert without interactive:
-openssl req -new -x509 -days 365 -key fd.key -out fd.crt \
-    -subj "/C=GB/L=London/O=Feisty Duck Ltd/CN=www.feistyduck.com"""
-
-!!!
-When a certificate contains alternative names, all common names are ignored. Newer
-certificates produced by CAs may not even include any common names. For that
-reason, include all desired hostnames on the alternative names list.
-!!!
-
-subjectAltName = DNS:*.feistyduck.com, DNS:feistyduck.com
-
-openssl x509 -req -days 365 \
-    -in fd.csr -signkey fd.key -out fd.crt \
-    -extfile fd.ext
-
 The Basic Constraints extension is used to mark certificates as belonging to a CA, giving them
 the ability to sign other certificates. Non-CA certificates will either have this extension omit-
 ted or will have the value of CA set to FALSE . 
@@ -54,3 +32,10 @@ openssl x509 -inform PEM -in fd.pem -outform DER -out fd.der
 
 openssl ciphers -V switch to
 request extra-verbose output. In this mode, the output will also contain suite IDs,
+
+#compare speed
+openssl speed rc4 aes rsa ecdh sha
+openssl speed -evp aes-128-cbc
+openssl speed aes-128-cbc
+
+
