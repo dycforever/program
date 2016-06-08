7d4fda1197d49d5f040684daa5ba0db4af706b42
dingyc <dingyc@localhost.localdomain>
2016-06-07 21:05:04 -0400

    ci setup_br and clean_br

--- virt_exp/bridge/clean_br.sh
+++ virt_exp/bridge/clean_br.sh
@@ -1,6 +1,11 @@
 
-sudo ip route del 10.1.0.0/16 
+
 sudo ip link set dev br down
 sudo brctl delbr br
 sudo ip netns del net1
 
+
+sudo ip link set dev eth0 down
+sudo ip addr add 192.168.171.128/24 dev eth0
+sudo ip link set dev eth0 up
+sudo ip route add default via 192.168.171.2 dev eth0
