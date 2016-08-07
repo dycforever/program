e2faaff07a92afde781f57dff59324fdd92dc710
dycforever <carldyc@gmail.com>
2016-08-07 15:05:03 +0800

    fix pki-example

--- openssl/pki-example/generate_cert.sh
+++ openssl/pki-example/generate_cert.sh
@@ -5,6 +5,13 @@ then
     echo "usage: $0 cn"
     exit 1
 fi
+
+# create root and signing ca
+sh script/create_root_req.sh
+sh script/create_root_cert.sh
+sh script/create_ca_req.sh
+sh script/create_ca_cert.sh
+
 #common name, such as host *.sm.cn
 CERT_CN=$1 sh script/create_sm_req.sh
 sh script/create_sm_cert.sh
--- openssl/pki-example/recreate.sh
+++
@@ -1,6 +0,0 @@
-
-sh script/create_root_req.sh
-sh script/create_root_cert.sh
-sh script/create_ca_req.sh
-sh script/create_ca_cert.sh
-
